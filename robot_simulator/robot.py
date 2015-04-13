b59aad6bc4cedf64a93818eeacfa7cd1557e1647
Jesse Hon <honkalong@gmail.com>
2015-04-13 17:23:44 +1000

    Implement Robot

--- robot_simulator/robot.py
+++ robot_simulator/robot.py
@@ -1,2 +1,28 @@
 class Robot(object):
-    pass
+    def __init__(self, board):
+        self._board = board
+        self._position = None
+        self._direction = None
+
+    @property
+    def board(self):
+        return self._board
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        if not self._board.rect.contains(position):
+            raise Exception('Invalid move')
+
+        self._position = position
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, direction):
+        self._direction = direction
