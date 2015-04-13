a90a63964f210f96ceb2e467c4e303783fa4d727
Jesse Hon <honkalong@gmail.com>
2015-04-13 16:49:39 +1000

    Implement Board with Rect component

--- robot_simulator/board.py
+++ robot_simulator/board.py
@@ -1,2 +1,5 @@
 class Board(object):
-    pass
+    def __init__(self, size_x, size_y):
+        bottom_left = Point(0, 0)
+        top_right = Point(size_x - 1, size_y -1)
+        self.rect = Rect(bottom_left, top_right)
