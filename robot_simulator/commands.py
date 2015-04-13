ca791ee2ed3953b746295645e34896813d7cfdbf
Jesse Hon <honkalong@gmail.com>
2015-04-13 11:44:58 +1000

    Create abstract BaseCommand

---
+++ robot_simulator/commands.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+
+class BaseCommand()
+    __metaclass__ = ABCMeta
+    _identifier = ""
+    _args = []
+
+    def __init__(args=None):
+        self.args = args
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def args(self):
+        return self._args
+
+    @args.setter
+    def args(self, values):
+        self._args = values
+
+    @abstractmethod
+    def invoke(target):
+        pass
