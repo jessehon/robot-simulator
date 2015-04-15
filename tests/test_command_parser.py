c312d52655263342cbf7da086f9ce72fc15ef207
Jesse Hon <honkalong@gmail.com>
2015-04-15 22:42:22 +1000

    Fix renamed parse_line function

--- tests/test_command_parser.py
+++ tests/test_command_parser.py
@@ -6,26 +6,26 @@ class TestCommandParser():
         self.commandParser = CommandParser()
 
     def test_place(self):
-        command = self.commandParser.parse_line("PLACE 0,1,NORTH")
+        command = self.commandParser.parse("PLACE 0,1,NORTH")
         assert command.identifier == "PLACE"
         assert command.params == ["0", "1", "NORTH"]
 
     def test_move(self):
-        command = self.commandParser.parse_line("MOVE")
+        command = self.commandParser.parse("MOVE")
         assert command.identifier == "MOVE"
 
     def test_left(self):
-        command = self.commandParser.parse_line("LEFT")
+        command = self.commandParser.parse("LEFT")
         assert command.identifier == "LEFT"
 
     def test_right(self):
-        command = self.commandParser.parse_line("RIGHT")
+        command = self.commandParser.parse("RIGHT")
         assert command.identifier == "RIGHT"
 
     def test_report(self):
-        command = self.commandParser.parse_line("REPORT")
+        command = self.commandParser.parse("REPORT")
         assert command.identifier == "REPORT"
 
     @raises(CommandNotFoundError)
     def test_invalid_command(self):
-        command = self.commandParser.parse_line("TEST")
+        command = self.commandParser.parse("TEST")
